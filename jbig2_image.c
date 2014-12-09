ffeeeb5892eb3822c135e00bad42cbc533798707
Yuriy M. Kaminskiy <yumkam@gmail.com>
2014-12-10 00:37:08 +0300

    jbig2_image_new: integer overflow check is off-by-one

--- jbig2_image.c
+++ jbig2_image.c
@@ -46,14 +46,14 @@ jbig2_image_new(Jbig2Ctx *ctx, int width, int height)
 
     stride = ((width - 1) >> 3) + 1;    /* generate a byte-aligned stride */
     /* check for integer multiplication overflow */
-    check = ((int64_t) stride) * ((int64_t) height);
+    check = ((int64_t) stride) * ((int64_t) height) + 1;
     if (check != (int)check) {
         jbig2_error(ctx, JBIG2_SEVERITY_FATAL, -1, "integer multiplication overflow from stride(%d)*height(%d)", stride, height);
         jbig2_free(ctx->allocator, image);
         return NULL;
     }
     /* Add 1 to accept runs that exceed image width and clamped to width+1 */
-    image->data = jbig2_new(ctx, uint8_t, (int)check + 1);
+    image->data = jbig2_new(ctx, uint8_t, (int)check);
     if (image->data == NULL) {
         jbig2_error(ctx, JBIG2_SEVERITY_FATAL, -1, "could not allocate image data buffer! [stride(%d)*height(%d) bytes]", stride, height);
         jbig2_free(ctx->allocator, image);
