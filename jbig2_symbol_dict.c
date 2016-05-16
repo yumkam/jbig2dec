ArtifexSoftware/ghostpdl

commit 7feea483a0354e13c42ffa239b9b0c2d00f08428

Bug 693050 : Fix error handling in 9557 folder

jbig2_symbol_dict.c
@@ -966,16 +966,19 @@ jbig2_symbol_dictionary(Jbig2Ctx *ctx, Jbig2Segment *segment,
     if (flags & 0x000c) {
       jbig2_error(ctx, JBIG2_SEVERITY_FATAL, segment->number,
 		  "SDHUFF is zero, but contrary to spec SDHUFFDH is not.");
+      goto cleanup;
     }
     if (flags & 0x0030) {
       jbig2_error(ctx, JBIG2_SEVERITY_FATAL, segment->number,
 		  "SDHUFF is zero, but contrary to spec SDHUFFDW is not.");
+      goto cleanup;
     }
   }
 
   if (flags & 0x0080) {
       jbig2_error(ctx, JBIG2_SEVERITY_FATAL, segment->number,
         "bitmap coding context is used (NYI) symbol data likely to be garbage!");
+      goto cleanup;
   }
 
   /* 7.4.2.1.2 */

yumkam/jbig2dec

commit 10b3ff98026d7e877bd21085bff559bae04c9d58

jbig2_symbol_dictionary: remove duplicate and incorrect fatal error
1) "Bitmap coding context used" is bit 8 (0x0100), not bit 7 (0x0080);
Bit 7 indicates (supported) SDHUFFAGGINST feature;

2) With that fixed, it will be duplicate of same check at more proper place below (also handled as fatal error/NYI)

Regression by 7feea483a0354e13c42ffa239b9b0c2d00f08428

jbig2_symbol_dict.c
@@ -920,11 +920,6 @@ jbig2_symbol_dictionary(Jbig2Ctx *ctx, Jbig2Segment *segment, const byte *segmen
         }
     }
 
-    if (flags & 0x0080) {
-        jbig2_error(ctx, JBIG2_SEVERITY_FATAL, segment->number, "bitmap coding context is used (NYI) symbol data likely to be garbage!");
-        goto cleanup;
-    }
-
     /* 7.4.2.1.2 */
     sdat_bytes = params.SDHUFF ? 0 : params.SDTEMPLATE == 0 ? 8 : 2;
     memcpy(params.sdat, segment_data + 2, sdat_bytes);